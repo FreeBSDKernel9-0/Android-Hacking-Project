77beff1ac1a973bf5f3dc215e10c98bb9f9ef6f0
FreeBSDKernel9-0 <coconutrepairs@gmail.com>
2026-06-30 17:51:03 +0100

    ro-debuggable.sh, my new .sh script!

---
+++ Fire Stick/ro-debuggable.sh
@@ -0,0 +1,11 @@
+#! /bin/mksh
+
+pm list packages
+
+VERSION=$(getprop ro.build.version.release)
+
+echo "Amazon's Android version: $VERSION"
+
+ps -A | grep root
+
+(echo "The shell guy finally posted a script!")
