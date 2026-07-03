6813224e29ee12e44ee864841474a23e422a95fc
FreeBSDKernel9-0 <coconutrepairs@gmail.com>
2026-07-03 14:29:54 +0100

    Adding in a fully patched Security Patch Level: date counter.

--- Fire Stick/ro-build-description.sh
+++ Fire Stick/ro-build-description.sh
@@ -6,6 +6,12 @@ getprop ro.build.display.id
 getprop ro.build.version.release
 getprop ro.build.version.incremental
 
+ P=$(getprop ro.build.version.security_patch); Y=${P:0:4}; M_NUM=${P:5:2}; D=${P:8:2}
+M="January  February March    April    May      June     July     August   SeptemberOctober  November December "
+RAW_M=${M:$(( (${M_NUM#0} - 1) * 9 )):9}; set -- $RAW_M; CLEAN_D=$(( ${D#0} ))
+case $CLEAN_D in 11|12|13) S=th ;; *) case $(( CLEAN_D % 10 )) in 1) S=st ;; 2) S=nd ;; 3) S=rd ;; *) S=th ;; esac ;; esac
+echo "Security Patch Level: $1 ${CLEAN_D}${S}, $Y" # Security patch level stuff, mine's December 1st, 2022.
+
 # How to see your browser info, and to see if it has trust issues:
 content query --uri content://settings/secure --projection name:value | grep -i "agent"
  settings get global http_agent # HTTP Agent - mine's just NULL
