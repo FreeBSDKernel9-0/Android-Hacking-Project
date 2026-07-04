2571689c3167826bf6594133421dfa288ce50a4d
FreeBSDKernel9-0 <coconutrepairs@gmail.com>
2026-07-04 06:23:53 +0100

    Temporary save.

--- Fire Stick/BrokenAccessControlChain.sh
+++ Fire Stick/BrokenAccessControlChain.sh
@@ -5,9 +5,9 @@ settings put secure accessibility_speak_password 1 # Improper Access Control in 
 settings put system screen_off_timeout 2103 # Sets how long the screen stays on - SYSTEM table.
 settings put system sound_effects_enabled 0 # Permanently disables system audio - SYSTEM table.
 
-# Crashing the screensaver: Bypassing BIND_DREAM_SERVICE using CWE 755 (Improper Handling Of System Disruptions), and CWE 862 (Missing Authorization):
-am broadcast -a android.intent.action.DREAMING_STARTED
-am force-stop com.amazon.ftv.screensaver
+# Crashing the YouTube and Netflix apps by panicking the Android System server, using CWE 755 (Improper Handling Of System Disruptions), and CWE 862 (Missing Authorization):
+am start -n com.amazon.firetv.youtube/dev.cobalt.app.MainActivity -f 0x14000000 && sleep 2.0 && (logcat -c && am broadcast -a android.intent.action.USER_PRESENT && am force-stop com.amazon.firetv.youtube) && dumpsys window | grep -E 'mCurrentFocus|mFocusedApp'
+am start -n com.netflix.ninja/.MainActivity -f 0x14000000 && sleep 2.0 && (logcat -c && am broadcast -a android.intent.action.USER_PRESENT && am force-stop com.netflix.ninja) && dumpsys window | grep -E 'mCurrentFocus|mFocusedApp'
 
 # Reading all SQLite3 database tables using CWE 284:
 settings list system
